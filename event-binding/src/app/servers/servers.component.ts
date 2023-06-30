c468952d93e7a439baaf5c05e5399786e3aa4b00
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-06-30 15:04:48 -0700

    Add files via upload

---
+++ event-binding/src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  template: `
+    // <app-server></app-server>
+    // <app-server></app-server>`,
+  templateUrl:"./servers.component.html",
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No Server Was Created!';
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } ,2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server Was Created!';
+  }
+}
+  
