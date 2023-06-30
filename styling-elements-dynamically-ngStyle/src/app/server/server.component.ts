c4b5d1d6f922294e5fa3f5821be78bb8d3169403
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-06-30 16:16:51 -0700

    Add files via upload

---
+++ styling-elements-dynamically-ngStyle/src/app/server/server.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  constructor(){
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+  getServerStatus() {
+    return this.serverStatus;
+  }
+  getColor() {
+    return this.serverStatus === 'online' ? 'red' : 'green';
+  }
+}
---
+++ styling-elements-dynamically-ngStyle/src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Enter Server Name';
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Is... ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
+
