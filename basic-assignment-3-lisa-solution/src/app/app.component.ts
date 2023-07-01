f50377a662554cc6c7b49f278e368020b2139a17
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-07-01 13:56:28 -0700

    Add files via upload
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

---
+++ basic-assignment-3-lisa-solution/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    // @ts-ignore
+    this.showSecret = !this.showSecret;
+    // @ts-ignore
+    this.log.push(this.log.length + 1);
+  }
+}
