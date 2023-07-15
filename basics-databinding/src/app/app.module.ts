9f289acb03b77fe76aa06245dafd10cbbcc4ecc1
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-07-15 14:34:40 -0700

    Update app.module.ts
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- basics-databinding/src/app/app.module.ts
+++ basics-databinding/src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-
-import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {ServerComponent} from './server/server.component';
+import {ServersComponent} from './servers/servers.component';
 
 @NgModule({
   declarations: [
@@ -20,4 +18,5 @@ import { ServersComponent } from './servers/servers.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
