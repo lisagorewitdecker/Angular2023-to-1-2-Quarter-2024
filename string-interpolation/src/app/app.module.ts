b5a8d6e399dc9daf3e9fa2f310611face6384a21
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-07-15 15:17:48 -0700

    Update app.module.ts
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- string-interpolation/src/app/app.module.ts
+++ string-interpolation/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -20,4 +18,5 @@ import { ServersComponent } from './servers/servers.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
