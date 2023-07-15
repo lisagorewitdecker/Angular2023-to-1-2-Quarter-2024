af0d74d20758ba1e6a15ca81d8dffd42e51a8fdf
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-07-15 15:41:52 -0700

    Update servers.component.ts
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

--- property-binding-vs-string-interpolation/src/app/servers/servers.component.ts
+++ property-binding-vs-string-interpolation/src/app/servers/servers.component.ts
@@ -7,18 +7,18 @@ import { Component, OnInit } from '@angular/core';
   template: `
     // <app-server></app-server>
     // <app-server></app-server>`,
-  templateUrl:"./servers.component.html",
+  templateUrl: "./servers.component.html",
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  constructor() { 
+
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    } ,2000);
+    }, 2000);
   }
 
   ngOnInit() {
   }
-
 }
