eebed9e4301ba7524bdbf489605118bfc690c19c
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-07-17 19:04:00 -0700

    Add files via upload
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

---
+++ prj-start-b/src/app/recipes/recipes-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is Simply a Test', 'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg')
+  ];
+  constructor () { }
+  ngOnInit() {
+
+  }
+}
