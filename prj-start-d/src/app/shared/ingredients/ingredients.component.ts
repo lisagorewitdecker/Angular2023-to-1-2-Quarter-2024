daceeb219be9bc0b1e7fc8dd1ef70fa98407335c
Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>
2023-07-21 14:26:42 -0700

    Add files via upload
    Signed-off-by: Lisa Gorewit-Decker <50784106+lisagorewitdecker@users.noreply.github.com>

---
+++ prj-start-d/src/app/shared/ingredients/ingredients.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Ingredient } from '../ingredients/ingredient.model';
+import { RecipesComponent } from '../../recipes/recipes.component';
+
+@Component({
+  selector: 'app-ingredients',
+  templateUrl: './ingredients.component.html',
+  styleUrls: ['./ingredients.component.css']
+})
+
+export class RecipesComponent {
+
+}
